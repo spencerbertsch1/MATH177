c090064024be8a7a765da75f74ac86e032899f30
Spencer Bertsch <spencerbertsch99@gmail.com>
2021-12-22 17:24:53 -0500

    add r basics script

---
+++ assignment0/r_practice.R
@@ -0,0 +1,22 @@
+# demo r script 
+# spencer bertsch 
+# December 2021
+
+# get the working directory 
+wd <- getwd()
+print(wd)
+
+# set the working directory 
+setwd('/Users/spencerbertsch/Desktop/dev/r_practice')
+
+df <- read.csv('demo_data/stateData.csv')
+
+# subset the data by one of the columns 
+northeast_states <- subset(df, state.region == 1)
+
+# subset by more than one field
+northeast_states2 <- df[((df$state.region == 1) & 
+                           (df$population > 1000) & 
+                           (df$income > 1000)), ]
+
+
