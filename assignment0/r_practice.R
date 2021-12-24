2168268b8de79cc2bc291fc9859505fa47deadb4
Spencer Bertsch <spencerbertsch99@gmail.com>
2021-12-24 11:24:16 -0500

    add section on factor (categorical) variables

--- assignment0/r_practice.R
+++ assignment0/r_practice.R
@@ -2,6 +2,8 @@
 # spencer bertsch 
 # December 2021
 
+library(ggplot2)
+
 # get the working directory 
 wd <- getwd()
 print(wd)
@@ -19,5 +21,20 @@ northeast_states2 <- df[((df$state.region == 1) &
                            (df$population > 1000) & 
                            (df$income > 1000)), ]
 
-# print('something')
+reddit_df <- read.csv('assignment0/demo_data/reddit.csv')
+
+# factor variables are just categorical variables - these commends can tell us about them
+str(reddit_df)
+
+# look at how many categories there are in the employment status
+table(reddit_df$employment.status)
+summary(reddit_df)
+
+# let's look at the different categorical levels of the age.range variable 
+levels(reddit_df$age.range)
+
+# we can move the column "Under 18" to the first position
+reddit_df$age.range <- relevel(reddit_df$age.range, "Under 18")
+
+qplot(data=reddit_df, x=age.range)
 
